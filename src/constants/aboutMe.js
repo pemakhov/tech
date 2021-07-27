8abfbbc77944a8167ea6f202fdd3e7a37c64c418
pemakhov <serhiy.pemakhov@gmail.com>
2021-07-27 22:48:03 +0300

    refactor: change about me

--- src/constants/aboutMe.js
+++ src/constants/aboutMe.js
@@ -3,5 +3,6 @@
 export const aboutMe = [
   'I am a junior front end / back end web developer.',
   'Finding out how the modern websites work and ability to build the whole thing were extremely interesting for me. Therefore I refrained from specializing on the front end or back end yet, but preferred to try out both.',
-  'At the moment, I don\'t have an opportunity to develop on the fulltime basis, but I try to always be involved into some kind of project to keep theoretical knowledge and get useful skills.',
+  'At the moment, I don\'t have an opportunity to develop on the fulltime basis, but I try to always be involved into some kind of project to keep theoretical knowledge and get useful skills. Every day I spend several hours writing the code.',
+  'If you are looking for a performer who will be passionate about your project and the required intensity is not extremely high, I believe I will not let you down.'
 ];

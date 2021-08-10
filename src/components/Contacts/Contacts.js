65932b14b3a84fce7e581aa585cfd008f563d889
Сергій Пемахов <pemakhov@kbp.radiy.com>
2021-08-10 17:12:08 +0300

    refactor: work with styles

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -7,9 +7,24 @@ const Contacts = () => {
       <div id="waveContacts" className="wave" />
       <section className="contacts">
         <div className="container">
-          <p>email: {contacts.email}</p>
-          <p>telegram: {contacts.telegram}</p>
-          <p>skype: {contacts.skype}</p>
+          <p>
+            <span className="contactsIcon">
+              <i className="fas fa-envelope fa-sm" />
+            </span>
+            {contacts.email}
+          </p>
+          <p>
+            <span className="contactsIcon">
+              <i className="fab fa-telegram-plane fa-sm" />
+            </span>
+            {contacts.telegram}
+          </p>
+          <p>
+            <span className="contactsIcon">
+              <i className="fab fa-skype fa-sm" />
+            </span>
+            {contacts.skype}
+          </p>
         </div>
       </section>
     </>

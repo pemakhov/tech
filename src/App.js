1aeab86882cb86812896b12f6f5663faaa61452c
pemakhov <serhiy.pemakhov@gmail.com>
2022-01-16 12:09:28 +0200

    chore: remove change site title on the fly

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Home from './pages/Home/Home';
 import '../node_modules/normalize.css/normalize.css';
 import '../node_modules/@fortawesome/fontawesome-free/js/brands';
 import '../node_modules/@fortawesome/fontawesome-free/js/solid';
 import '../node_modules/@fortawesome/fontawesome-free/js/fontawesome';
 
-const App = () => {
-  useEffect(() => {
-    document.title = 'Portfolio';
-  }, []);
-
-return <Home />
-};
+const App = () => <Home />;
 
 export default App;
